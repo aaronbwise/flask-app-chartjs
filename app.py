212cdb31d8fe0ec891e657fae32b020182136ae9
Aaron Wise <aaronbwise@gmail.com>
2020-09-11 05:26:08 +0700

    add database

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    # Add development database -> won't be used in Heroku
-    # NEED TO UPDATE/MANAGE
+    # development database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:P@ssw1rd@localhost/budget'
 else:
+    # deployment database
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Create database instance
 db = SQLAlchemy(app)
 
 posts = [
